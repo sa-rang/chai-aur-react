a5c3051cbb5e5a49c19dd6891904547a81dff2bd
Sarang Chaudhary <schaudhary@aiopsgroup.com>
2023-12-20 11:57:22 +0530

    theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 
-import { useState } from 'react';
-import BgOptions from "./components/BgOptions"
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from './contexts/theme';
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+
 
 function App() {
-  const [color, setColor] = useState("olive");
+  const [themeMode, setThemeMode] = useState("light")
 
-  const handleColorChange = (iData) => {
-    console.log("ok", iData)
-    setColor(iData)
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
   }
 
+  useEffect(() => {
+    const htmlEl = document.querySelector('html');
+    htmlEl.classList.remove("light", "dark");
+    htmlEl.classList.add(themeMode)
+
+  }, [themeMode])
+
   return (
-    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }} >
-      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <BgOptions handleChange={handleColorChange} />
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
